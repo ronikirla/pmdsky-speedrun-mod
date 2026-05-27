86d6722558d3675448862ed9d561d2b8e6636dfa
Roni Kirla <roni.kirla@gmail.com>
2026-05-27 23:52:46 +0300

    In main menu with custom timer at zero, advance start_time to keep displayed timer at 0

--- src/igt_modifications.c
+++ src/igt_modifications.c
@@ -1,6 +1,7 @@
 #include <pmdsky.h>
 #include <cot.h>
 #include "custom_headers.h"
+#include "timer.h"
 
 bool is_saving = false;
 
@@ -9,7 +10,16 @@ __attribute__((used)) uint8_t PlayTimerTickAndWaitTillVBlank(void) {
   // If not, then advance the play timer. Otherwise pause (normal behaviour)
   int* main_menu_magic = (int*) 0x22a3670;
   if (*main_menu_magic != 0x22a3e94 && PLAY_TIME_SECONDS != 0) {
+    // In main menu with custom timer at zero, advance start_time to keep displayed timer at 0
+    struct play_time* igt = (struct play_time*) &PLAY_TIME_SECONDS;
+    bool advance_start_time = false;
+    if (IGTDifferenceFrames(igt, &start_time) == 0) {
+      advance_start_time = true;
+    }
     PlayTimerTick((struct play_time*) &PLAY_TIME_SECONDS);
+    if (advance_start_time) {
+      memcpy(&start_time, igt, sizeof(struct play_time));
+    }
   }
   return WaitTillVBlank();
 }
